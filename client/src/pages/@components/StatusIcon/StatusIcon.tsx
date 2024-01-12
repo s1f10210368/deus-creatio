3611147a9791b16f468a0c9625f52573cb9294e7
s1f10210368 <s1f102103684@iniad.org>
2024-01-12 21:40:20 +0900

    add exportIcon

--- client/src/pages/@components/StatusIcon/StatusIcon.tsx
+++ client/src/pages/@components/StatusIcon/StatusIcon.tsx
@@ -1,7 +1,7 @@
 import type { AppModel } from '$/commonTypesWithClient/appModels';
 import styles from './statusIcon.module.css';
 
-const CheckMark = () => {
+export const CheckMark = () => {
   return (
     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <circle cx="10" cy="10" r="10" fill="#14b869" />
@@ -10,7 +10,7 @@ const CheckMark = () => {
   );
 };
 
-const CrossMark = () => {
+export const CrossMark = () => {
   return (
     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <circle cx="10" cy="10" r="10" fill="#ec0000" />
